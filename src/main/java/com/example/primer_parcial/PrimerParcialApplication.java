9159703edab5e396b8102d5bf04dd417f1d07286
SCIS3-PC08\SCIS3-08 <hividemi-8134@yopmail.com>
2022-10-06 18:46:08 -0500

    archivos iniciales

---
+++ src/main/java/com/example/primer_parcial/PrimerParcialApplication.java
@@ -0,0 +1,13 @@
+package com.example.primer_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/primer_parcial/PrimerParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primer_parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
