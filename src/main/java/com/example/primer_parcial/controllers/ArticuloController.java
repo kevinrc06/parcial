de0e03fb3598d5a8af0295d4a3eebb6912644f1c
Shirly <sjtrigosr@ufpso.edu.co>
2022-12-08 14:29:14 -0500

    Añadi el CrossOrigin para el funcionamiento

--- src/main/java/com/example/primer_parcial/controllers/ArticuloController.java
+++ src/main/java/com/example/primer_parcial/controllers/ArticuloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class ArticuloController {
 
--- src/main/java/com/example/primer_parcial/controllers/AuthController.java
+++ src/main/java/com/example/primer_parcial/controllers/AuthController.java
@@ -4,10 +4,11 @@ import com.example.primer_parcial.models.Usuario;
 import com.example.primer_parcial.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class AuthController {
     @Autowired
