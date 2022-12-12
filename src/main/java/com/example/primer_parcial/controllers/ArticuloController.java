6eb722f68ebdaab65319a37edc2f6126e029cc9f
Kevin <kjriverac@ufpso.edu.co>
2022-12-12 17:15:24 -0500

    reajustes de algunos empoints

--- src/main/java/com/example/primer_parcial/controllers/ArticuloController.java
+++ src/main/java/com/example/primer_parcial/controllers/ArticuloController.java
@@ -51,7 +51,7 @@ public class ArticuloController {
 
         return articuloService.allArticulosByName(nombre);
     }
-    @GetMapping("/articulo/{codigo}")
+    @GetMapping("/articulo/codigo/{codigo}")
     public ResponseEntity listaPorCodigo(@PathVariable String codigo,@RequestHeader(value = "Authorization") String token){
         try {
             if(jwtUtil.getKey(token) == null) {
@@ -64,7 +64,7 @@ public class ArticuloController {
 
     }
 
-    @PutMapping("/articulo/{codigo}")
+    @PutMapping("/articulo/codigo/{codigo}")
     public ResponseEntity modificarArticulo(@PathVariable String codigo, @RequestBody Articulo articulo,@RequestHeader(value = "Authorization") String token) {
         try {
             if(jwtUtil.getKey(token) == null) {
--- src/main/java/com/example/primer_parcial/controllers/CategoriaController.java
+++ src/main/java/com/example/primer_parcial/controllers/CategoriaController.java
@@ -8,9 +8,13 @@ import com.example.primer_parcial.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -31,6 +35,7 @@ public class CategoriaController {
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token no valido");
         }
+
 
     }
     @GetMapping("/categorias")
@@ -61,6 +66,19 @@ public class CategoriaController {
         }
         return categoriaService.allCategoriasByName(nombre);
 
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 
 }
